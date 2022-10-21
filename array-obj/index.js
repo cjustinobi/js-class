d20e52586fc07826a46480f7ad735f51286c3aab
Justin <justin@pop-os.localdomain>
2022-10-21 12:25:25 +0100

    Complete Array obj topic

--- array-obj/index.js
+++ array-obj/index.js
@@ -1,64 +1,76 @@
-const students = localStorage.getItem('students') ?
-    JSON.parse(localStorage.getItem('students')) : []
+let students = localStorage.getItem('students') ?
+  JSON.parse(localStorage.getItem('students')) : []
 
 let editMode = false
 let indexToEdit = undefined
 
 
 function submitForm(e) {
-    const name = document.getElementById('name').value
-    const phone = document.getElementById('phone').value
-    const village = document.getElementById('village').value
+  const name = document.getElementById('name').value
+  const phone = document.getElementById('phone').value
+  const village = document.getElementById('village').value
 
-    const obj = {name: name, phone: phone, village: village } /* ES5 */
-    // const obj = { name, phone, village } ES6
+  const obj = {
+    id:
+    name: name,
+    phone: phone,
+    village: village
+  } /* ES5 */
+  // const obj = { name, phone, village } ES6
 
-    if (editMode) {
-        students[indexToEdit] = obj
-    } else {
-        students.push(obj)
-    }
+  if (editMode) {
+    students[indexToEdit] = obj
+  } else {
+    students.push(obj)
+  }
 
 
-    localStorage.setItem('students', JSON.stringify(students))
+  localStorage.setItem('students', JSON.stringify(students))
 
-    displayData(students)
-    reset()
+  displayData(students)
+  reset()
 
 }
 
 function editStudent(i) {
-    editMode = true
-    indexToEdit = i
-    const student = students[i]
+  editMode = true
+  indexToEdit = i
+  const student = students[i]
 
-    document.getElementById('name').value = student.name
-    document.getElementById('phone').value = student.phone
-    document.getElementById('village').value = student.village
+  document.getElementById('name').value = student.name
+  document.getElementById('phone').value = student.phone
+  document.getElementById('village').value = student.village
+}
+
+function deleteStudent(i) {
+  students = students.filter((student, index) => index !== i )
+  localStorage.setItem('students', JSON.stringify(students))
+  displayData(students)
 }
 
 
 function reset() {
-    editMode = false
+  editMode = false
 
-    document.getElementById('name').value = ''
-    document.getElementById('phone').value = ''
-    document.getElementById('village').value = ''
+  document.getElementById('name').value = ''
+  document.getElementById('phone').value = ''
+  document.getElementById('village').value = ''
 }
 
 function displayData(data) {
 
-    let el = '<div>'
-    data.forEach((item, index) => {
-        el += `<p>
+  let el = '<div>'
+  data.forEach((item, index) => {
+    el += `<p>
             ${item.name} - 
             ${item.phone} - 
             ${item.village} <br />
             <button type="button" onclick="editStudent(${index})">Edit</button>
+            <button type="button" onclick="deleteStudent(${index})">Delete</button>
     </p>`
-    })
-    el += '</div>'
-    document.getElementById('data').innerHTML = el
+  })
+  el += '</div>'
+  document.getElementById('data').innerHTML = el
 }
 
 displayData(students)
