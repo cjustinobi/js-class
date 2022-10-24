87a1af55d74d2890650d4cf2e5e75c131d9e7b3e
Justin <justin@pop-os.localdomain>
2022-10-24 16:04:47 +0100

    Working with data

--- array-obj/index2.js
+++
@@ -1,7 +0,0 @@
-let details = prompt('Enter your name, phone and address. (Format: name, phone, address)')
-details = details.split(',')
-console.log(details)
-
-let name = details[0]
-let phone = details[1]
-let villa = details[2]
